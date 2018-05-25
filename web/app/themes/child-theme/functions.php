afd435ea6507fda0038be4712596270caddcda34
Daniel Ryan <d@dryan.com>
2018-05-25 17:24:26 -0400

    radcliffe theme and child theme setup

--- web/app/themes/child-theme/functions.php
+++ web/app/themes/child-theme/functions.php
@@ -1,9 +1,6 @@
 <?php
 function a4f_enqueue_styles() {
-
-    $parent_style = 'radcliffe-2-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
